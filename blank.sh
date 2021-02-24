3ba8f8673d126aef55e06f462952699504acac98
currypeter16 <78630148+currypeter16@users.noreply.github.com>
2021-02-24 10:13:49 -0500

    Update blank.sh

--- blank.sh
+++ blank.sh
@@ -17,8 +17,7 @@ sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo apt install xfce4-terminal nano -y
-sudo update-alternatives --config x-terminal-emulator
+sudo apt install nautilus nano -y
 sudo adduser ROCK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
