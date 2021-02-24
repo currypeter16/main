43fe8bd4615dcbb8bf1815123ee26aa2c8daab02
currypeter16 <78630148+currypeter16@users.noreply.github.com>
2021-02-24 08:47:01 -0500

    Update blank.sh

--- blank.sh
+++ blank.sh
@@ -18,6 +18,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install xfce4-terminal nano -y
+sudo update-alternatives --config x-terminal-emulator
 sudo adduser ROCK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
